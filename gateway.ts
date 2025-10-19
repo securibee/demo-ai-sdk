c8c01330053e7fc191a688596f0af088c2617f9e
securibee <securibee@gmail.com>
2025-10-19 15:42:45 -0400

    initial commit

---
+++ gateway.ts
@@ -0,0 +1,21 @@
+import { streamText } from 'ai';
+import 'dotenv/config';
+
+async function main() {
+  const model = 'anthropic/claude-haiku-4.5'
+  const result = streamText({
+    model: model,
+    prompt: 'Invent a new holiday and describe its traditions.',
+  });
+
+  for await (const textPart of result.textStream) {
+    process.stdout.write(textPart);
+  }
+
+  console.log();
+  console.log('Token usage:', await result.usage);
+  console.log('Finish reason:', await result.finishReason);
+  console.log('Model used:', model);
+}
+
+main().catch(console.error);
